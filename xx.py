2d6d5a20f1011cefad3dca00912c9b8f7ddd140b
BadCoder228 <167223943+BadCoder228@users.noreply.github.com>
2024-04-21 18:10:10 +0700

    Update xx.py

--- xx.py
+++ xx.py
@@ -4,12 +4,13 @@ from telebot import types as ty
 import time
 import art
 on_off = True
+Flag_rob =False
 art.tprint('BOT IS ONLINE')
 print('SEvaORG©')
 m5 = q.Client(q.CallbackAPIVersion.VERSION1)
 m5.connect("broker.hivemq.com", 1883, 300)
 m5.subscribe('testopic/2',0)
-bot = t.TeleBot('7195327169:AAG2UXyEqFFu0DT9HnxpsZZAwbP9khiwAsM')
+bot = t.TeleBot('7195327169:AAEcq2FV2AZJqPqFS2RxsG9XnnJIOXmNyFQ')
 @bot.message_handler(commands=['comms', 'start'])
 def on_message(message):
     markup = ty.InlineKeyboardMarkup(row_width=1)
@@ -21,9 +22,32 @@ def on_message(message):
     i6 = ty.InlineKeyboardButton('отключиться', callback_data='reb')
     i = ty.InlineKeyboardButton('прямо(по прямой)', callback_data='str')
     markup.add(i,i1,i2,i3,i4,i5,i6)
-    bot.send_message(message.chat.id, 'перед тем как бездумно тыкать на кнопочки, зайдите сюда-> https://www.hivemq.com/demos/websocket-client/, а после создайте заголовок "testopic/2".Ну а чтобы получить код на РОбота просто пропишите ниже комманду "/code", а если вы все сделали, то зацените эти комманды: "/chBg", "/assis_msgs_onoff"', reply_markup=markup)
-@bot.message_handler(commands=['chBg'])
+    bot.send_message(message.chat.id, 'перед тем как бездумно тыкать на кнопочки, зайдите сюда-> https://www.hivemq.com/demos/websocket-client/, а после создайте заголовок "testopic/2".Ну а чтобы получить код на РОбота просто пропишите ниже комманду "/code", а если вы все сделали, то зацените эти комманды:\n "/chBg" - меняет цвет заливки,\n "/assis_msgs_onoff"- вкл/выкл сообщения о роботе\n"/chSm" - меняет мордашку\n"/chSmONOFF" - права на изменение мордашки', reply_markup=markup)
+@bot.message_handler(commands=['chSm'])
 def chSm(message):
+    if Flag_rob ==True:
+      bot.send_message(message.chat.id, 'Введите эмоцию на робота из 3-х или меньше символов:')
+      @bot.message_handler(content_types=['text']) 
+      def message_input_step(message):
+        if Flag_rob ==True:
+          global tttext  
+          tttext = message.text
+          m5.publish('testopic/2', tttext[:3])
+          bot.send_message(message.chat.id,'(/установлен каастомный текст)✅sucess(ive sent da command)')
+      bot.register_next_step_handler(message, message_input_step)
+    else:
+      bot.send_message(message.chat.id, 'включите права на изменение мордашки!(когда не надо выключайте)')
+@bot.message_handler(commands=['chSmONOFF'])
+def sss(message):
+  global Flag_rob
+  if Flag_rob == True:
+    Flag_rob = False
+    bot.send_message(message.chat.id, 'изменение мордашки выключено, для включения -> "/chSmONOFF"\nк командам -> "/comms"')
+  else:
+    Flag_rob = True
+    bot.send_message(message.chat.id, 'изменение мордашки включено, для выключения -> "/chSmONOFF"\nк командам -> "/comms"')
+@bot.message_handler(commands=['chBg'])
+def chBg(message):
   chbg= ty.InlineKeyboardMarkup(row_width=1)
   q1 =  ty.InlineKeyboardButton('красный', callback_data='re')
   q2 =  ty.InlineKeyboardButton('оранжевый', callback_data='or')
@@ -65,43 +89,43 @@ def code(message):
         '        wait_ms(1000)',
         '        m.set_motor_speed(2, 0)',
         '        m.set_motor_speed(1, 0)',
-      "    elif topic_data=='re':",
+      "    elif topic_data=='redd':",
         '        l.setColor(0xffff00)',
         '        l0.setColor(0x5400f5)',
         '        setScreenColor(0xff0000)',
         '        l.show()',
         '        l0.show()',
-      "    elif topic_data=='or':",
+      "    elif topic_data=='oran':",
         '        l.setColor(0xff0400)',
         '        l0.setColor(0x5400f5)',
         '        setScreenColor(0xff8700)',
         '        l.show()',
         '        l0.show()',
-      "    elif topic_data=='ye':",
+      "    elif topic_data=='yell':",
         '        l.setColor(0xff0400)',
         '        l0.setColor(0x5400f5)',
         '        setScreenColor(0xffff00)',
         '        l.show()',
         '        l0.show()',
-      "    elif topic_data=='gr':",
+      "    elif topic_data=='gree':",
         '        l.setColor(0xff0400)',
         '        l0.setColor(0x5400f5)',
         '        setScreenColor(0x00ff33)',
         '        l.show()',
         '        l0.show()',
-      "    elif topic_data=='cy':",
+      "    elif topic_data=='cyan':",
         '        l.setColor(0xff0400)',
         '        l0.setColor(0x5400f5)',
         '        setScreenColor(0x00ffe2)',
         '        l.show()',
         '        l0.show()',
-      "    elif topic_data=='bl':",
+      "    elif topic_data=='blue':",
         '        l.setColor(0xff0400)',
         '        l0.setColor(0xff0000)',
         '        setScreenColor(0x2c00ff)',
         '        l.show()',
         '        l0.show()',
-      "    elif topic_data=='pu':",
+      "    elif topic_data=='purp':",
         '        l.setColor(0xff0400)',
         '        l0.setColor(0xff0000)',
         '        setScreenColor(0xdb00ff)',
@@ -113,6 +137,8 @@ def code(message):
         '        wait_ms(7000)',
         '        m.set_motor_speed(2, 0)',
         '        m.set_motor_speed(1, 0)',
+      "    elif len(topic_data) <=3:",
+        '        l0.setText(topic_data)',
       "    elif topic_data=='behind':",
         '        m.set_motor_speed(2, -90)',
         '        m.set_motor_speed(1, -90)',
@@ -144,16 +170,6 @@ def code(message):
     'w.start()']
     text228 = '\n'.join(txt)
     bot.reply_to(message, '# а вот и он!:\n\n'+text228+'\n\n# а теперь скопируйте его и вставьте сюда:"https://flow.m5stack.com/", перейдите во вкладку "</>python", вставьте код и после нажмите кнопку "run" (ничего не удалять не надо) и перейдите в телеграмм.После этих действий пропишите комманду "/comms", и управляйте им на здоровье.(!ЕСЛИ В М5 ПОПРОСЯТ КОД ТО ВВЕДИТЕ: D58D8813!)(SEvaORG©)' )
-'''@bot.message_handler(commands=['chSm'])
-def chSm(message):
-  chsm= ty.InlineKeyboardMarkup(row_width=1)
-  q1 =  ty.InlineKeyboardButton('красный', callback_data='re')
-  q2 =  ty.InlineKeyboardButton('оранжевый', callback_data='or')
-  q3 =  ty.InlineKeyboardButton('желтый', callback_data='ye')
-  q4 =  ty.InlineKeyboardButton('зеленый', callback_data='gr')
-  q5 =  ty.InlineKeyboardButton('голубой', callback_data='cy')
-  q6 =  ty.InlineKeyboardButton('синий', callback_data='bl')
-  q7 =  ty.InlineKeyboardButton('фиолетовый', callback_data='pu')'''
 @bot.callback_query_handler(func=lambda call:True)
 def call1(call):
     if call.message:
@@ -193,37 +209,37 @@ def call1(call):
             if on_off == True:
               bot.send_message(call.message.chat.id, '(/перезагрузка)✅sucess(ive sent da command)')
         elif call.data == 're':
-            m5.publish('testopic/2', 're')
+            m5.publish('testopic/2', 'redd')
             print('(/установлен фон:красный)✅sucess(ive sent da command)')
             if on_off == True:
               bot.send_message(call.message.chat.id, '(/установлен фон:красный)✅sucess(ive sent da command)')
         elif call.data == 'or':
-            m5.publish('testopic/2', 'or')
+            m5.publish('testopic/2', 'oran')
             print('(/установлен фон:оранжевый)✅sucess(ive sent da command)')
             if on_off == True:
               bot.send_message(call.message.chat.id, '(/установлен фон:оранжевый)✅sucess(ive sent da command)')
         elif call.data == 'ye':
-            m5.publish('testopic/2', 'ye')
+            m5.publish('testopic/2', 'yell')
             print('(/установлен фон:желтый)✅sucess(ive sent da command)')
             if on_off == True:
               bot.send_message(call.message.chat.id, '(/установлен фон:желтый)✅sucess(ive sent da command)')
         elif call.data == 'gr':
-            m5.publish('testopic/2', 'gr')
+            m5.publish('testopic/2', 'gree')
             print('(/установлен фон:зеленый)✅sucess(ive sent da command)')
             if on_off == True:
               bot.send_message(call.message.chat.id, '(/установлен фон:зеленый)✅sucess(ive sent da command)')
         elif call.data == 'cy':
-            m5.publish('testopic/2', 'cy')
+            m5.publish('testopic/2', 'cyan')
             print('(/установлен фон:голубой)✅sucess(ive sent da command)')
             if on_off == True:
               bot.send_message(call.message.chat.id, '(/установлен фон:голубой)✅sucess(ive sent da command)')
         elif call.data == 'bl':
-            m5.publish('testopic/2', 'bl')
+            m5.publish('testopic/2', 'blue')
             print('(/установлен фон:синий)✅sucess(ive sent da command)')
             if on_off == True:
               bot.send_message(call.message.chat.id, '(/установлен фон:синий)✅sucess(ive sent da command)')
         elif call.data == 'pu':
-            m5.publish('testopic/2', 'pu')
+            m5.publish('testopic/2', 'purp')
             print('(/установлен фон:фиолетовый)✅sucess(ive sent da command)')
             if on_off == True:
               bot.send_message(call.message.chat.id, '(/установлен фон:фиолетовый)✅sucess(ive sent da command)')
